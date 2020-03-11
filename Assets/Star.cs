78b85c5266b8a9d40f9e8e290924b99a04ddce7f
Alex Dimitrijevic <baiomurang@gmail.com>
2020-03-11 01:13:05 -0500

    Temperature Sensor

---
+++ Assets/Star.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public float influenceRadius = 5f;
+
+    public float temperature = 100; //temperature decreases linearly from stars
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, influenceRadius);
+    }
+}
---
+++ Assets/TemperatureSensor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureSensor : MonoBehaviour
+{
+
+    public float currentTemperature;
+
+    public Gradient tempGrad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        checkTemperature();
+    }
+
+    void checkTemperature()
+    {
+        //find all stars in the system, really clunky
+        GameObject[] stars = GameObject.FindGameObjectsWithTag("Star");
+        currentTemperature = 0; //reset it so it can do it every frame
+
+        if (stars != null) //if there is any
+        {
+
+            foreach (GameObject starGO in stars)
+            {
+                //get the star component
+                Star star = starGO.GetComponent<Star>();
+
+
+
+                //find if it in the influence radius of the star
+
+                float distance = Vector3.Distance(transform.position, starGO.transform.position);
+
+                if (distance <= star.influenceRadius)
+                {
+                    //within it, find influence
+
+                    float influence = 1 - distance / star.influenceRadius; //realistically should be.. quadratic? 
+
+                    float tempDelta = influence * star.temperature;
+
+                    currentTemperature += tempDelta;
+
+
+                }
+            }
+        }
+    }
+
+}
