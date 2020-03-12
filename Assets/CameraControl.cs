9c573ef0b992ba912ebae07ce1c5f1e45e0c65fd
Alex Dimitrijevic <baiomurang@gmail.com>
2020-03-12 02:20:47 -0500

    Ship Moves

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float speed = 0.035f;
+
+    public float zoomLevel = 5;
+    public float targetZoomLevel = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        if (mousePos.x < 100)
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z + speed);
+        }
+
+        if (mousePos.x > Screen.width - 100)
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z - speed);
+        }
+
+
+
+        if (mousePos.y < 100)
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z - speed);
+        }
+
+        if (mousePos.y > Screen.height - 100)
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z + speed);
+        }
+
+        targetZoomLevel -= Input.mouseScrollDelta.y;
+
+        zoomLevel = Mathf.Lerp(zoomLevel, targetZoomLevel, Time.deltaTime * 4);
+
+        if(Mathf.Approximately(targetZoomLevel,zoomLevel))
+        {
+            targetZoomLevel = zoomLevel;
+        }
+
+        /*
+
+        if(targetZoomLevel <= 1)
+        {
+            transform.eulerAngles = new Vector3(Mathf.LerpAngle(transform.eulerAngles.x,15f,Time.deltaTime * 2), 45f, 0);
+        } else
+        {
+            transform.eulerAngles = new Vector3(Mathf.LerpAngle(transform.eulerAngles.x, 45f, Time.deltaTime * 4), 45f, 0);
+        }
+
+        */
+
+        Camera.main.orthographicSize = zoomLevel;
+
+
+    }
+}
---
+++ Assets/ShipController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public ResourceTank tank;
+
+    public GameObject target;
+
+    public GameObject[] planets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+
+        findNewTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion q = Quaternion.LookRotation(target.transform.position - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 64f * Time.deltaTime);
+
+        transform.Translate(Vector3.forward * Time.deltaTime);
+
+
+        if(Vector3.Distance(transform.position,target.transform.position) <= 1f) //arrived
+        {
+            Debug.Log("Arrived");
+
+            if (target.GetComponent<PlanetResource>().reciever) //sell ur shit
+            {
+                target.GetComponent<PlanetResource>().count += tank.count;
+                tank.count = 0;
+
+            }
+            else
+            {
+                if (target.GetComponent<PlanetResource>().count > 8)
+                {
+                    tank.count += 8;
+                    target.GetComponent<PlanetResource>().count -= 8;
+                }
+                else
+                {
+                    tank.count += target.GetComponent<PlanetResource>().count;
+                    target.GetComponent<PlanetResource>().count = 0;
+                }
+            }
+
+
+
+            
+            findNewTarget();
+        }
+
+
+    }
+
+    void findNewTarget()
+    {
+        foreach (GameObject planetGO in planets)
+        {
+            //find the best planet to go to
+            if (tank.count == 0) //empty, need to fill up
+            {
+                if (planetGO.GetComponent<PlanetResource>().reciever == false)  //can take resources
+                {
+                    target = planetGO;
+                }
+            } else
+            {
+                if (planetGO.GetComponent<PlanetResource>().reciever == true)  //can take resources
+                {
+                    target = planetGO;
+                }
+            }
+        }
+    }
+}
